0bde4f9559aba1b48f25ca33689bdd4cc75910e8
Abigail Howell <aahowel3@asu.edu>
2025-01-30 13:09:06 -0800

    Update preprocess_variants_forimputation_fordemuxlet.R

--- Pooled_Multiome_SingleCell/preprocess_variants_forimputation_fordemuxlet.R
+++ Pooled_Multiome_SingleCell/preprocess_variants_forimputation_fordemuxlet.R
@@ -15,18 +15,36 @@ library(SNPlocs.Hsapiens.dbSNP155.GRCh38)
 #bed file for plink is binary file of genotypes - not a normal bed 
 
 
-bim = read.csv("nPOD_UFDIchip_renamed.bim")
+library(genio)
+bim = read_bim("nPOD_UFDIchip_renamed.bim", verbose = TRUE)
+bim_split <- data.frame(do.call('rbind', strsplit(as.character(bim$id),';',fixed=TRUE)))
+#col split is odd bc some rows have 1-3 rsids
+varlist = as.data.frame(unlist(c(bim_split$X1, bim_split$X2, bim_split$X3,bim_split$X4,bim_split$X5,bim_split$X6,bim_split$X7)))
+varlist_un=unique(varlist)
+varlist_final = dplyr::filter(varlist_un, !grepl("Aff", `unlist(c(bim_split$X1, bim_split$X2, bim_split$X3, bim_split$X4, bim_split$X5, bim_split$X6, bim_split$X7))`))
+
+return_37 = snpsById(snps_37, varlist_final$`unlist(c(bim_split$X1, bim_split$X2, bim_split$X3, bim_split$X4, bim_split$X5, bim_split$X6, bim_split$X7))`, ifnotfound="drop")
+return_37= as.data.frame(return_37)
+
+return_38 = snpsById(snps_38, varlist_final$`unlist(c(bim_split$X1, bim_split$X2, bim_split$X3, bim_split$X4, bim_split$X5, bim_split$X6, bim_split$X7))`, ifnotfound="drop")
+return_38= as.data.frame(return_38)
 
 
-# Read in the supporting data
-snps <- SNPlocs.Hsapiens.dbSNP155.GRCh38
-snpcount(snps)
-seqinfo(snps)
-
-# Look up SNP positions
-my_snps <- snpsById(snps, mvp.cred$CS.SNP[str_detect(mvp.cred$CS.SNP, 'rs')], ifnotfound='warning')
-my_snps
-
-
+##checking for lines in bim file that have more than 1 rsid seperate by a ;
+ch <- ";"
+count <- sapply(as.character(bim$id), 
+                function(x, letter = ch){
+                  str <- strsplit(x, split = "")
+                  sum(unlist(str) == letter)
+                })
+print ("Count of !")
+# returning the number of occurrences
+bim$count = count
+bim_issue = bim[bim$count > 1, ]
+#checking for alt/ref columns that are INDELs based on character count
+bim_issue$alt_coutn = nchar(bim_issue$alt)
+bim_issue$ref_coutn = nchar(bim_issue$ref)
+bim_issue$combined = bim_issue$alt_coutn + bim_issue$ref_coutn
+bim_sevissue = bim_issue[bim_issue$combined == 2, ]
 
 
